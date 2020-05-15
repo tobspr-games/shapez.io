44668a85865ee0193b873ed07fc214e0d1fbbb78
tobspr <>
2020-05-15 09:09:37 +0200

    Remove source maps and adjust targets

--- src/js/webworkers/background_animation_frame_emittter.worker.js
+++ src/js/webworkers/background_animation_frame_emittter.worker.js
@@ -1,12 +1,12 @@
 // We clamp high deltas so 30 fps is fairly ok
-const bgFps = 30;
-const desiredMsDelay = 1000 / bgFps;
+var bgFps = 30;
+var desiredMsDelay = 1000 / bgFps;
 
 let lastTick = 0;
 
 function tick() {
-    const now = performance.now();
-    const delta = now - lastTick;
+    var now = performance.now();
+    var delta = now - lastTick;
     lastTick = now;
 
     // @ts-ignore
