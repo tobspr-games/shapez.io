70b38ffc0712022ae7f97903e48042c3b596bbce
tobspr <>
2020-05-23 09:19:56 +0200

    Improve mobile warning

--- src/js/states/mobile_warning.js
+++ src/js/states/mobile_warning.js
@@ -9,22 +9,20 @@ export class MobileWarningState extends GameState {
 
     getInnerHTML() {
         return `
-        
-            <div class="mobileWarning">
-                <img class="logo" src="${cachebust("res/logo.png")}" alt="shapez.io Logo">
+    
+            <img class="logo" src="${cachebust("res/logo.png")}" alt="shapez.io Logo">
 
-                <p>
-                    I'm sorry, but shapez.io is not yet available on mobile devices!
-                    (There is also no estimate when this will change, but feel to make a contribution! It's
-                    &nbsp;<a href="https://github.com/tobspr/shapez.io" target="_blank">open source</a>!)</p>
-                
-                <p>If you want to play on your computer, you can also get the standalone on itch.io:</p>
+            <p>
+                I'm sorry, but shapez.io is not yet available on mobile devices!
+                (There is also no estimate when this will change, but feel to make a contribution! It's
+                &nbsp;<a href="https://github.com/tobspr/shapez.io" target="_blank">open source</a>!)</p>
+            
+            <p>If you want to play on your computer, you can also get the standalone on itch.io:</p>
 
-                
-                <a href="${
-                    THIRDPARTY_URLS.standaloneStorePage
-                }" class="standaloneLink" target="_blank">Get the shapez.io standalone!</a>
-            </div>
+            
+            <a href="${
+                THIRDPARTY_URLS.standaloneStorePage
+            }" class="standaloneLink" target="_blank">Get the shapez.io standalone!</a>
         `;
     }
 
