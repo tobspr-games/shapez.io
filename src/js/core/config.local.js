5ee1766e9434081c779224f3de08be1f72d4782b
Dimava <dimava2@ya.ru>
2020-06-13 19:43:29 +0300

    fix debug setting menu

--- src/js/core/config.local.js
+++ src/js/core/config.local.js
@@ -2,86 +2,89 @@ export default {
     // You can set any debug options here!
     /* dev:start */
     // -----------------------------------------------------------------------------------
+    // Import debug settings from game settings
+    enableDebugSettings: true,
+    // -----------------------------------------------------------------------------------
     // Quickly enters the game and skips the main menu - good for fast iterating
-    // fastGameEnter: true,
+    fastGameEnter: false,
     // -----------------------------------------------------------------------------------
     // Skips any delays like transitions between states and such
-    // noArtificialDelays: true,
+    noArtificialDelays: false,
     // -----------------------------------------------------------------------------------
     // Disables writing of savegames, useful for testing the same savegame over and over
-    // disableSavegameWrite: true,
+    disableSavegameWrite: false,
     // -----------------------------------------------------------------------------------
     // Shows bounds of all entities
-    // showEntityBounds: true,
+    showEntityBounds: false,
     // -----------------------------------------------------------------------------------
     // Shows arrows for every ejector / acceptor
-    // showAcceptorEjectors: true,
+    showAcceptorEjectors: false,
     // -----------------------------------------------------------------------------------
     // Disables the music (Overrides any setting, can cause weird behaviour)
-    // disableMusic: true,
+    disableMusic: false,
     // -----------------------------------------------------------------------------------
     // Do not render static map entities (=most buildings)
-    // doNotRenderStatics: true,
+    doNotRenderStatics: false,
     // -----------------------------------------------------------------------------------
     // Allow to zoom freely without limits
-    // disableZoomLimits: true,
+    disableZoomLimits: false,
     // -----------------------------------------------------------------------------------
     // Shows a border arround every chunk
-    // showChunkBorders: true,
+    showChunkBorders: false,
     // -----------------------------------------------------------------------------------
     // All rewards can be unlocked by passing just 1 of any shape
-    // rewardsInstant: true,
+    rewardsInstant: false,
     // -----------------------------------------------------------------------------------
     // Unlocks all buildings
-    // allBuildingsUnlocked: true,
+    allBuildingsUnlocked: false,
     // -----------------------------------------------------------------------------------
     // Disables cost of bluepirnts
-    // blueprintsNoCost: true,
+    blueprintsNoCost: false,
     // -----------------------------------------------------------------------------------
     // Disables cost of upgrades
-    // upgradesNoCost: true,
+    upgradesNoCost: false,
     // -----------------------------------------------------------------------------------
     // Disables the dialog when completing a level
-    // disableUnlockDialog: true,
+    disableUnlockDialog: false,
     // -----------------------------------------------------------------------------------
     // Disables the simulation - This effectively pauses the game.
-    // disableLogicTicks: true,
+    disableLogicTicks: false,
     // -----------------------------------------------------------------------------------
     // Test the rendering if everything is clipped out properly
-    // testClipping: true,
+    testClipping: false,
     // -----------------------------------------------------------------------------------
     // Allows to render slower, useful for recording at half speed to avoid stuttering
-    // framePausesBetweenTicks: 1,
+    framePausesBetweenTicks: 1,
     // -----------------------------------------------------------------------------------
     // Replace all translations with emojis to see which texts are translateable
-    // testTranslations: true,
+    testTranslations: false,
     // -----------------------------------------------------------------------------------
     // Enables an inspector which shows information about the entity below the curosr
-    // enableEntityInspector: true,
+    enableEntityInspector: false,
     // -----------------------------------------------------------------------------------
     // Enables ads in the local build (normally they are deactivated there)
-    // testAds: true,
+    testAds: false,
     // -----------------------------------------------------------------------------------
     // Disables the automatic switch to an overview when zooming out
-    // disableMapOverview: true,
+    disableMapOverview: false,
     // -----------------------------------------------------------------------------------
     // Disables the notification when there are new entries in the changelog since last played
-    // disableUpgradeNotification: true,
+    disableUpgradeNotification: false,
     // -----------------------------------------------------------------------------------
     // Makes belts almost infinitely fast
-    // instantBelts: true,
+    instantBelts: false,
     // -----------------------------------------------------------------------------------
     // Makes item processors almost infinitely fast
-    // instantProcessors: true,
+    instantProcessors: false,
     // -----------------------------------------------------------------------------------
     // Makes miners almost infinitely fast
-    // instantMiners: true,
+    instantMiners: false,
     // -----------------------------------------------------------------------------------
     // When using fastGameEnter, controls whether a new game is started or the last one is resumed
-    // resumeGameOnFastEnter: false,
+    resumeGameOnFastEnter: false,
     // -----------------------------------------------------------------------------------
     // Special option used to render the trailer
-    // renderForTrailer: true,
+    renderForTrailer: false,
     // -----------------------------------------------------------------------------------
     /* dev:end */
 };
