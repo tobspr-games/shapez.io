756bcdb47313ca54fcea46f03988ccc16e6cd820
tobspr <tobias.springer1@googlemail.com>
2020-07-06 14:19:46 +0200

    Fix tunnels entrances connecting to exits sometimes when they shouldn't

--- src/js/changelog.js
+++ src/js/changelog.js
@@ -14,6 +14,7 @@ export const CHANGELOG = [
             "Updated the soundtrack again, it is now 20 minutes in total!",
             "Updated and added new translations (Thanks to all contributors!)",
             "Show confirmation when cutting area which is too expensive to get pasted again (by isaisstillalive)",
+            "Fix tunnels entrances connecting to exits sometimes when they shouldn't",
         ],
     },
     {
