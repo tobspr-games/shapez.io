35f77416ba00c983cef9a78255d477dc89a7bc98
tobspr <tobias.springer1@googlemail.com>
2020-07-07 13:34:49 +0200

    Update translations to latest version

--- sync-translations.js
+++ sync-translations.js
@@ -17,7 +17,7 @@ const originalContents = fs
 
 const original = YAML.parse(originalContents);
 
-const placeholderRegexp = /<([a-zA-Z_0-9]+)>/gi;
+const placeholderRegexp = /[[<]([a-zA-Z_0-9]+)[\]<]/gi;
 
 function match(originalObj, translatedObj, path = "/") {
     for (const key in originalObj) {
