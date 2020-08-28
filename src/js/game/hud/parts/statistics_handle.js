5336467838689f84d3e686fd5c4f28e2dd7e1039
tobspr <tobias.springer1@googlemail.com>
2020-08-28 22:41:13 +0200

    statistics are now shown in shapes / second instead of shapes / minute

--- src/js/game/hud/parts/statistics_handle.js
+++ src/js/game/hud/parts/statistics_handle.js
@@ -92,15 +92,10 @@ export class HUDShapeStatisticsHandle {
                     (this.root.productionAnalytics.getCurrentShapeRate(dataSource, this.definition) /
                         globalConfig.analyticsSliceDurationSeconds) *
                     60;
-                this.counter.innerText = T.ingame.statistics.shapesPerMinute.replace(
+                this.counter.innerText = T.ingame.statistics.shapesPerSecond.replace(
                     "<shapes>",
-                    formatBigNumber(rate)
+                    formatBigNumber(rate / 60)
                 );
-
-                if (G_IS_DEV && globalConfig.debug.detailedStatistics) {
-                    this.counter.innerText = "" + round2Digits(rate / 60) + " /s";
-                }
-
                 break;
             }
         }
