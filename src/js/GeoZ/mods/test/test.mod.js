dcda55123313e64452e4129ad134f10b42368f79
Exund <SylverProfessionnel@gmail.com>
2020-09-10 00:05:54 +0200

    Internal test mod update

--- src/js/GeoZ/mods/test/test.mod.js
+++ src/js/GeoZ/mods/test/test.mod.js
@@ -3,9 +3,9 @@ import { Vector } from "../../../core/vector";
 import { Entity } from "../../../game/entity";
 
 class MetaTestBuilding extends GeoZ.MetaModBuilding {
-	static getId() {
-		return "test";
-	}
+    static getId() {
+        return "test";
+    }
 
     static getKeybinding() {
         return "0";
@@ -13,17 +13,17 @@ class MetaTestBuilding extends GeoZ.MetaModBuilding {
 
     static getTranslations() {
         return {
-			variants: {
-				default: {
-					name: "Test",
-					description: "Test GeoZ building"
-				}
-			}, 
-			keybinding: "Test"
-		};
-	}
-	
-	constructor() {
+            variants: {
+                default: {
+                    name: "Test",
+                    description: "Test GeoZ building",
+                },
+            },
+            keybinding: "Test",
+        };
+    }
+
+    constructor() {
         super("test");
     }
 
@@ -35,17 +35,44 @@ class MetaTestBuilding extends GeoZ.MetaModBuilding {
         return new Vector(1, 1);
     }
 
-    getSpriteMeta() {
-		return {url:"https://raw.githubusercontent.com/Exund/shapez.io/master/res_raw/sprites/wires/boolean_false.png", width: 64, height: 64};
-	}
+    /**
+     * @returns {import("../../mod_building").BuildingSpriteMetas}
+     */
+    getSpriteMetas() {
+        const normal = {
+            url: "https://raw.githubusercontent.com/Exund/shapez.io/master/res_raw/sprites/wires/boolean_false.png",
+            width: 64,
+            height: 64,
+        };
 
-	setupEntityComponents() {}
+        return {
+            default: [
+                {
+                    normal,
+                    blueprint: normal,
+                },
+            ],
+        };
+    }
+
+    setupEntityComponents() {}
 }
 
 /**@type {GeoZ.Mod}*/
 const test = {
-	name: "test",
-	buildings: [MetaTestBuilding]
+    name: "test",
+    buildings: [MetaTestBuilding],
+    shapes: [
+        {
+            id: "leaf",
+            code: "F",
+            draw: "M 0 0 v 0.5 a 0.5 0.5 0 0 0 0.5 0.5 h 0.5 v -0.5 a 0.5 0.5 0 0 0 -0.5 -0.5 z",
+            tier: 2,
+            spawnData: {
+                color: "yellow",
+            },
+        },
+    ],
 };
 
 export default test;
