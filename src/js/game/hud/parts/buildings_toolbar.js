a4b46f852730ab0c8622ee7ef46207f1676db4de
tobspr <tobias.springer1@googlemail.com>
2020-09-24 16:37:44 +0200

    Further adjustments

--- src/js/game/hud/parts/buildings_toolbar.js
+++ src/js/game/hud/parts/buildings_toolbar.js
@@ -32,10 +32,10 @@ export class HUDBuildingsToolbar extends HUDBaseToolbar {
             ],
             secondaryBuildings: [
                 MetaStorageBuilding,
+                MetaReaderBuilding,
                 MetaLeverBuilding,
                 MetaFilterBuilding,
                 MetaDisplayBuilding,
-                MetaReaderBuilding,
             ],
             visibilityCondition: () =>
                 !this.root.camera.getIsMapOverlayActive() && this.root.currentLayer === "regular",
