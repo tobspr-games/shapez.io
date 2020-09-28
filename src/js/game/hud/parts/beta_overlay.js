25135056f713776d9b1523c6ce4a9bb49c935d60
tobspr <tobias.springer1@googlemail.com>
2020-09-28 18:20:57 +0200

    update beta overlay

--- src/js/game/hud/parts/beta_overlay.js
+++ src/js/game/hud/parts/beta_overlay.js
@@ -7,7 +7,7 @@ export class HUDBetaOverlay extends BaseHUDPart {
             parent,
             "ingame_HUD_BetaOverlay",
             [],
-            "<h2>CLOSED BETA VERSION</h2><span>This version is unstable, might crash and is not final!</span>"
+            "<h2>UNSTABLE BETA VERSION</h2><span>Steam Release: 9th October 2020!</span>"
         );
     }
 
