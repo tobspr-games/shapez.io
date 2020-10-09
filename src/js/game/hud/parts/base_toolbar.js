c7428d6f77ba9696c1a40b1e6acfcc453ea98f30
EmeraldBlock <69981203+EmeraldBlock@users.noreply.github.com>
2020-10-08 20:17:08 -0500

    oops, need an import

--- src/js/game/hud/parts/base_toolbar.js
+++ src/js/game/hud/parts/base_toolbar.js
@@ -1,6 +1,6 @@
 import { gMetaBuildingRegistry } from "../../../core/global_registries";
 import { Signal, STOP_PROPAGATION } from "../../../core/signal";
-import { makeDiv } from "../../../core/utils";
+import { makeDiv, safeModulo } from "../../../core/utils";
 import { KEYMAPPINGS } from "../../key_action_mapper";
 import { MetaBuilding } from "../../meta_building";
 import { GameRoot } from "../../root";
