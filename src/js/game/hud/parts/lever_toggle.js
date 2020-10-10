85ff62fa78a2677b8760426751892f3dbfd11d63
3ddy <39034386+3ddyy@users.noreply.github.com>
2020-10-10 22:29:03 +0200

    fix button right click bug

--- src/js/game/hud/parts/lever_toggle.js
+++ src/js/game/hud/parts/lever_toggle.js
@@ -21,9 +21,6 @@ export class HUDLeverToggle extends BaseHUDPart {
                 if (button === enumMouseButton.left) {
                     leverComp.toggled = !leverComp.toggled;
                     return STOP_PROPAGATION;
-                } else if (button === enumMouseButton.right) {
-                    this.root.logic.tryDeleteBuilding(contents);
-                    return STOP_PROPAGATION;
                 }
             }
         }
