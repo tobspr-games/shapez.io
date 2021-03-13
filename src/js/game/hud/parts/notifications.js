777184661c8bf6c04ec3c947943a7444078cd668
DJ1TJOO <t.ferb1@gmail.com>
2021-03-13 23:18:20 +0100

    Added notification log

--- src/js/game/hud/parts/notifications.js
+++ src/js/game/hud/parts/notifications.js
@@ -2,6 +2,25 @@ import { makeDiv } from "../../../core/utils";
 import { T } from "../../../translations";
 import { BaseHUDPart } from "../base_hud_part";
 
+function makeDivElement(id = null, classes = [], innerHTML = "") {
+    const div = document.createElement("div");
+    if (id) {
+        div.id = id;
+    }
+    for (let i = 0; i < classes.length; ++i) {
+        div.classList.add(classes[i]);
+    }
+    div.innerHTML = innerHTML;
+    return div;
+}
+
+function makeDivAfter(sibling, id = null, classes = [], innerHTML = "") {
+    const div = makeDivElement(id, classes, innerHTML);
+    if (sibling.nextElementSibling) sibling.parentNode.insertBefore(div, sibling.nextElementSibling);
+    else sibling.parentNode.appendChild(div);
+    return div;
+}
+
 /** @enum {string} */
 export const enumNotificationType = {
     saved: "saved",
@@ -14,6 +33,13 @@ const notificationDuration = 3;
 export class HUDNotifications extends BaseHUDPart {
     createElements(parent) {
         this.element = makeDiv(parent, "ingame_HUD_Notifications", [], ``);
+
+        this.hoverElement = makeDiv(this.element, "notificationHover");
+        this.hoverElement.style.minHeight = "0";
+        this.hoverElement.style.padding = "0";
+        this.hoverElement.style.margin = "0";
+
+        this.paddingElement = makeDiv(this.element, "notificationPadding");
     }
 
     initialize() {
@@ -21,6 +47,8 @@ export class HUDNotifications extends BaseHUDPart {
 
         /** @type {Array<{ element: HTMLElement, expireAt: number}>} */
         this.notificationElements = [];
+
+        this.visibleNotificationElements = [];
 
         // Automatic notifications
         this.root.signals.gameSaved.add(() =>
@@ -33,22 +61,24 @@ export class HUDNotifications extends BaseHUDPart {
      * @param {enumNotificationType} type
      */
     onNotification(message, type) {
-        const element = makeDiv(this.element, null, ["notification", "type-" + type], message);
+        const element = makeDivAfter(this.hoverElement, null, ["notification", "type-" + type], message);
         element.setAttribute("data-icon", "icons/notification_" + type + ".png");
 
-        this.notificationElements.push({
+        const notification = {
             element,
             expireAt: this.root.time.realtimeNow() + notificationDuration,
-        });
+        };
+
+        if (this.hoverElement.hasAttribute("style")) this.hoverElement.removeAttribute("style");
+        this.visibleNotificationElements.push(this.notificationElements.push(notification) - 1);
     }
 
     update() {
         const now = this.root.time.realtimeNow();
-        for (let i = 0; i < this.notificationElements.length; ++i) {
-            const handle = this.notificationElements[i];
+        for (let i = 0; i < this.visibleNotificationElements.length; ++i) {
+            const handle = this.notificationElements[this.visibleNotificationElements[i]];
             if (handle.expireAt <= now) {
-                handle.element.remove();
-                this.notificationElements.splice(i, 1);
+                this.visibleNotificationElements.splice(i, 1);
             }
         }
     }
