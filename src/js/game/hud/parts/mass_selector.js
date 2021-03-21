3d6323e455b838b1f379473e8d74f2d9faf91f49
Aurumaker72 <armkdev72@gmail.com>
2021-03-21 15:20:41 +0100

    smh

--- src/js/game/hud/parts/mass_selector.js
+++ src/js/game/hud/parts/mass_selector.js
@@ -204,11 +204,11 @@ export class HUDMassSelector extends BaseHUDPart {
      * @param {enumMouseButton} mouseButton
      */
     onMouseDown(pos, mouseButton) {
-		if (
-		!this.root.keyMapper.getBinding(KEYMAPPINGS.massSelect.massSelectStart.pressed) ||
-        mouseButton !== enumMouseButton.left) {
-		    return;
-		}
+        if (
+        !this.root.keyMapper.getBinding(KEYMAPPINGS.massSelect.massSelectStart.pressed) ||
+            mouseButton !== enumMouseButton.left) {
+            return;
+        }
 
         if (!this.root.keyMapper.getBinding(KEYMAPPINGS.massSelect.massSelectSelectMultiple).pressed) {
             // Start new selection
