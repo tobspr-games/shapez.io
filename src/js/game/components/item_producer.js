a3fb3c432a67ce7b434cce3b5778f376ba5dcd1d
tobspr <tobias.springer1@gmail.com>
2021-05-25 12:09:54 +0200

    Fix another bug

--- src/js/game/components/item_producer.js
+++ src/js/game/components/item_producer.js
@@ -1,14 +1,7 @@
-import { types } from "../../savegame/serialization";
 import { Component } from "../component";
 
 export class ItemProducerComponent extends Component {
     static getId() {
         return "ItemProducer";
-    }
-
-    static getSchema() {
-        return {
-            type: types.string,
-        };
     }
 }
