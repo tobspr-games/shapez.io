1ef86b6870dbc05461cd537d3933e2d4fd3af83d
Sense101 <67970865+Sense101@users.noreply.github.com>
2021-08-29 12:04:55 +0100

    finished update to 1.4.3 code - Sense101

--- src/js/game/hud/parts/shape_tooltip.js
+++ src/js/game/hud/parts/shape_tooltip.js
@@ -73,53 +73,15 @@ export class HUDShapeTooltip extends BaseHUDPart {
             const ejectorComp = this.currentEntity.components.ItemEjector;
             const staticComp = this.currentEntity.components.StaticMapEntity;
 
-<<<<<<< HEAD
-            const context = parameters.context;
-=======
             const bounds = staticComp.getTileSize();
             const totalArea = bounds.x * bounds.y;
             const maxSlots = totalArea < 2 ? 1 : 1e10;
 
             let slotsDrawn = 0;
->>>>>>> master
 
             for (let i = 0; i < ejectorComp.slots.length; ++i) {
                 const slot = ejectorComp.slots[i];
 
-<<<<<<< HEAD
-                if (!slot.lastItem || slot.lastItem._type != "shape") {
-                    continue;
-                }
-
-                const drawPos = staticComp
-                    .localTileToWorld(slot.pos.add(enumDirectionToVector[slot.direction].multiplyScalar(1)))
-                    .toWorldSpaceCenterOfTile();
-
-                const slotCenterPos = staticComp
-                    .localTileToWorld(slot.pos.add(enumDirectionToVector[slot.direction].multiplyScalar(0.2)))
-                    .toWorldSpaceCenterOfTile();
-
-                context.fillStyle = THEME.shapeTooltip.outline;
-                context.strokeStyle = THEME.shapeTooltip.outline;
-
-                context.lineWidth = 1.5;
-                context.beginPath();
-                context.moveTo(slotCenterPos.x, slotCenterPos.y);
-                context.lineTo(drawPos.x, drawPos.y);
-                context.stroke();
-
-                context.beginCircle(slotCenterPos.x, slotCenterPos.y, 3.5);
-                context.fill();
-
-                context.fillStyle = THEME.shapeTooltip.background;
-                context.strokeStyle = THEME.shapeTooltip.outline;
-
-                context.lineWidth = 1.2;
-                context.beginCircle(drawPos.x, drawPos.y, 11 + 1.2 / 2);
-                context.fill();
-                context.stroke();
-                slot.lastItem.drawItemCenteredClipped(drawPos.x, drawPos.y, parameters, 22);
-=======
                 if (!slot.lastItem) {
                     continue;
                 }
@@ -132,7 +94,6 @@ export class HUDShapeTooltip extends BaseHUDPart {
                 const drawPos = staticComp.localTileToWorld(slot.pos).toWorldSpaceCenterOfTile();
 
                 slot.lastItem.drawItemCenteredClipped(drawPos.x, drawPos.y, parameters, 25);
->>>>>>> master
             }
         }
     }
