781273e91d4d44bdd9d43e4274fd781765772b19
tobspr <tobias.springer1@googlemail.com>
2022-06-16 12:10:09 +0200

    Add html element remove polyfill

--- src/js/core/polyfills.js
+++ src/js/core/polyfills.js
@@ -83,10 +83,30 @@ function objectPolyfills() {
     }
 }
 
+function domPolyfills() {
+    // from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+    (function (arr) {
+        arr.forEach(function (item) {
+            if (item.hasOwnProperty("remove")) {
+                return;
+            }
+            Object.defineProperty(item, "remove", {
+                configurable: true,
+                enumerable: true,
+                writable: true,
+                value: function remove() {
+                    this.parentNode.removeChild(this);
+                },
+            });
+        });
+    })([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+}
+
 function initPolyfills() {
     mathPolyfills();
     stringPolyfills();
     objectPolyfills();
+    domPolyfills();
 }
 
 function initExtensions() {
